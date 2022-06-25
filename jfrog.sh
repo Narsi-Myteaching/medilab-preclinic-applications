97e31b5d852fd60789bfc5135c1edab0d8309b30
Narsi-Myteaching <104019637+Narsi-Myteaching@users.noreply.github.com>
2022-06-25 20:47:07 +0530

    Create jfrog.sh
    adding the jfrog.sh

---
+++ jfrog.sh
@@ -0,0 +1,26 @@
+server=http://34.201.241.36:8082/artifactory
+repo=libs-snapshot-local
+
+userName=admin
+password=Adance123$
+
+# Maven artifact location
+name=medilab-morning-preclinic
+artifact=com/medilab/$name/0.0.1-SNAPSHOT
+path=$server/$repo/$artifact
+echo $path
+
+build=$(curl -u "admin":"Adance123$" $path/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/")
+echo $build
+
+extension=$(curl -u "admin":"Adance123$" $path/maven-metadata.xml | grep '<extension>' | head -2 | sed "s/.*<extension>\([^<]*\)<\/extension>.*/\2/")
+echo $extension
+
+artifact_name=$name-$build.war
+echo $artifact_name
+
+url=$path/$artifact_name
+
+# Download
+echo $url
+wget  --user=$userName --password=$password $url
